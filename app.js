4c284b0b2c841f533f5fe96b4cb519af681f1729
Cristopher Ramos <cristopher.lizandro.11@gmail.com>
2021-03-17 16:24:13 -0500

    cors solve test

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 
 app.use(function (req, res, next) {
 
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'https://compralocal.pe');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.setHeader('Access-Control-Allow-Credentials', true);
